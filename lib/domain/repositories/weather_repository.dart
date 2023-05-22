d93c1d5f18db3a8fa7c3cab7cd4dae15a4d1775f
Karolina Sadlik <truskaweczka518@gmail.com>
2023-05-22 22:19:14 +0200

    Add fromJson method to our model

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -10,17 +10,13 @@ class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    final responseData = await _weatherRemoteDataSource.getWeatherData(
+    final json = await _weatherRemoteDataSource.getWeatherData(
       city: city,
     );
-    if (responseData == null) {
+    if (json == null) {
       return null;
     }
-    final name = responseData['location']['name'] as String;
-    final temperature = (responseData['current']['temp_c'] + 0.0)
-        as double; // chcemy zeby wyswietlalo inty i double bo temp moze miec jedna lub dwie cyfry
-    // te finals powyżej odnosz się do danych z rest api z internetu, każdy z nich ma swoją grupę i trzeba podać ich przynależność
 
-    return WeatherModel(city: name, temperature: temperature);
+    return WeatherModel.fromJson(json);
   }
 }
